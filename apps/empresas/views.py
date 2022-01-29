04b1ad9fb95b105b3008e67db37de0fa348fd5d8
Ed Junior <juniore4500@gmmail.com>
2022-01-29 00:33:56 -0300

    formação do crud de empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.edit import CreateView
+from .models import Empresa
+
+
+class EmpresaCreate(CreateView):
+    model = Empresa
+
+    fields = ['nome']
+
+    success_url = '/'
+
+
+    def form_valid(self, form):
+        obj = form.save()
+
+        funcionario = self.request.user.funcionario
+
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return '/'
+
+
+
+
+
+
